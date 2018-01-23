1f8f3a9d5981f090647b27bfb8d3bdc150ad696b
WilliamKluge <klugewilliam@gmail.com>
2018-01-23 17:43:50 -0500

    Completed project 0

---
+++ prj/0/driver_proj0.java
@@ -0,0 +1,15 @@
+/**
+ * Main class for Project 0
+ * @author William Kluge
+ *   Contact: klugewilliam@gmail.com
+ */
+public class driver_proj0 {
+
+  /**
+   * Main method for Project 0
+   * @param args This program does not use arguments
+   */
+  public static void main(String[] args) {
+    System.out.print("Hello World!"); // Prints "Hello World!"
+  }
+}
