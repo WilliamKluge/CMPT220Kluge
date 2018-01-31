eef4a11732b8286555a8357b02ab5a023405cfa2
WilliamKluge <klugewilliam@gmail.com>
2018-01-30 20:03:41 -0500

    Finished labs 4.9 and 4.13

--- labs/2/driver_lab413.java
+++ labs/2/driver_lab413.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 /**
- * Program to print the number related to a character
+ * Program to check if a character is a vowel or a consonant
  *
  * @author William Kluge Contact: klugewilliam@gmail.com
  */
-public class driver_lab49 {
+public class driver_lab413 {
 
   public static void main(String[] args) {
     // Setup input
@@ -20,8 +20,22 @@ public class driver_lab49 {
       character = input.nextLine();
     }
 
-    // Print result
-    System.out.format("%s is %d", character.charAt(0), (int) (character.charAt(0)));
+    // Check and print result
+    String message;
+
+    switch (character.toLowerCase().charAt(0)) {
+      case 'a': // a
+      case 'e': // e
+      case 'i': // i
+      case 'o': // o
+      case 'u': // u
+        message = " is a vowel.";
+        break;
+      default:
+        message = " is a consonant.";
+    }
+
+    System.out.format(character + message);
 
   }
 
