965d07f813c071ba7a832548facda717c217469f
WilliamKluge <klugewilliam@gmail.com>
2018-02-02 14:31:50 -0500

    Finished lab 4.23

--- labs/2/driver_lab423.java
+++ labs/2/driver_lab423.java
@@ -26,9 +26,30 @@ public class driver_lab423 {
     System.out.print("Enter the hourly pay of this worker: ");
     double hourlyPay = input.nextDouble();
 
+    System.out.print("Enter federal tax withholding rate: ");
+    double federalTaxWithholding = input.nextDouble();
+
+    System.out.print("Enter state tax withholding rate: ");
+    double stateTaxWithholding = input.nextDouble();
+
+    // Calculations
+    double grossIncome = hoursWorked * hourlyPay;
+    double federalWithholding = grossIncome * federalTaxWithholding;
+    double stateHolding = grossIncome * stateTaxWithholding;
+    double totalDeduction = federalWithholding + stateHolding;
+    double netPay = grossIncome - totalDeduction;
+
     // Print payroll statement
-    System.out.format("%s worked %.2f hours for $%.2f", employee, hoursWorked,
-        hoursWorked * hourlyPay);
+    System.out.format("Employee Name: %s\n" +
+                    "Hours Worked: %.2f\n" +
+                    "Pay Rate: $%.2f\n" +
+                    "Gross Pay: $%.2f\n\n" +
+                    "Deductions:\n" +
+                    "\tFederal Withholding (%.2f): $%.2f\n" +
+                    "\tState Withholding (%.2f): $%.2f\n" +
+                    "\tTotal Deduction: $%.2f\n" +
+                    "Net Pay: $%.2f", employee, hoursWorked, hourlyPay, grossIncome, federalTaxWithholding * 100,
+            federalWithholding, stateTaxWithholding * 100, stateHolding, totalDeduction, netPay);
 
   }
 
