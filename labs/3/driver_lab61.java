dfa6b1538e084be5c2012a10a072bf3810af2ec1
WilliamKluge <klugewilliam@gmail.com>
2018-02-09 23:32:00 -0500

    Lab 6.1

--- labs/3/driver_lab61.java
+++ labs/3/driver_lab61.java
@@ -1,5 +1,5 @@
 /**
- * Program to
+ * Program to display the first 100 pentagonal numbers
  *
  * @author William Kluge Contact: klugewilliam@gmail.com
  */
@@ -12,6 +12,29 @@ public class driver_lab61 {
    */
   public static void main(String[] args) {
 
+    final int[][] table = new int[10][10];
+
+    for (int i = 0; i < 10; ++i) {
+      for (int j = 1; j <= 10; ++j) {
+        table[i][j - 1] = pentogonalNumber(i * 10 + j);
+      }
+    }
+
+    for (int[] row : table) {
+      System.out.format("%7d%7d%7d%7d%7d%7d%7d%7d%7d%7d\n",
+          row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
+    }
+
+  }
+
+  /**
+   * Returns the next pentagonal number
+   *
+   * @param n Number to find pentagonal for
+   * @return Pentagonal number associated with n
+   */
+  private static int pentogonalNumber(int n) {
+    return n * (3 * n - 1) / 2;
   }
 
 }
