bd95a3f08f9b89f088bdbbe5add23b93fb95a62b
William Kluge <klugewilliam@gmail.com>
2018-02-16 23:46:49 -0500

    6.17

--- labs/4/driver_lab617.java
+++ labs/4/driver_lab617.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Program to
  *
@@ -6,11 +9,41 @@
 public class driver_lab617 {
 
     /**
-     * Main method for lab
+     * Main method for lab 6.17
      *
      * @param args This program does not utilize arguments
      */
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = input.nextInt();
+
+        printMatrix(n);
+
+    }
+
+    /**
+     * Prints a matrix of 0s or 1s
+     * @param n Size of the n-by-n matrix
+     */
+    private static void printMatrix(int n) {
+
+        Random ran = new Random();
+
+        int[][] matrix = new int[n][n];
+
+        for (int[] row : matrix)
+            for (int i = 0; i <  row.length; ++i)
+                row[i] = ran.nextInt(2);
+
+        for (int[] row : matrix) {
+            for (int i : row)
+                System.out.print(i + " ");
+            System.out.print("\n");
+        }
+
 
     }
 }
