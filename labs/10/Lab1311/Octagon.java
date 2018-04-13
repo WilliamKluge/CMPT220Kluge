d08c9cfd7142f26509fc67a7d88c0789ead021a2
William Kluge <klugewilliam@gmail.com>
2018-04-13 12:56:48 -0400

    Added 13.9 UML and Lab 13.11

---
+++ labs/10/Lab1311/Octagon.java
@@ -0,0 +1,37 @@
+package Lab1311;
+
+public class Octagon extends GeometricObject implements Comparable<Octagon>, Cloneable {
+
+  private double side;
+
+  public Octagon() {
+    side = 0;
+  }
+
+  public Octagon(double side) {
+    this.side = side;
+  }
+
+  @Override
+  public double getArea() {
+    return (2 + 4 / Math.sqrt(2)) * side * side;
+  }
+
+  @Override
+  public double getPerimeter() {
+    return side * 8;
+  }
+
+  @Override
+  public int compareTo(Octagon o) {
+    return Double.compare(side, o.side);
+  }
+
+  public double getSide() {
+    return side;
+  }
+
+  public void setSide(double side) {
+    this.side = side;
+  }
+}
---
+++ labs/10/Lab1311/driver_lab1311.java
@@ -0,0 +1,22 @@
+package Lab1311;
+
+/**
+ * Program to test the Octagon class
+ *
+ * @author William Kluge Contact: klugewilliam@gmail.com
+ */
+public class driver_lab1311 {
+
+  /**
+   * Main method for lab 13.11
+   *
+   * @param args This program does not utilize arguments
+   */
+  public static void main(String[] args) {
+    Octagon octagonOne = new Octagon(10);
+    Octagon octagonTwo = new Octagon(20);
+
+    System.out.println("Area of First Octagon: " + octagonOne.getArea());
+    System.out.println("Comparison of the two octagons: " + octagonOne.compareTo(octagonTwo));
+  }
+}
